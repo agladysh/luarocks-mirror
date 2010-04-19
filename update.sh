2b1fb2432b4aee46718df67c11d5e2370329a92a
Alexander Gladysh <agladysh@gmail.com>
2010-04-19 17:14:57 +0400

    added rocks-cvs

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
-wget --mirror http://luarocks.org/repositories/rocks/
+wget --mirror http://luarocks.org/repositories/rocks/ -I 'repositories/rocks*'
+wget --mirror http://luarocks.org/repositories/rocks-cvs/ -I 'repositories/rocks-cvs*'
